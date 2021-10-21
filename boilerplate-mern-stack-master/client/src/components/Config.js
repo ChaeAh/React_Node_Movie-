5b89815660da8b8f9e9040d10e98f1b1e76d0ef9
ChaeAh <37362669+ChaeAh@users.noreply.github.com>
2021-10-21 18:18:49 +0900

    Delete Config.js

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++
@@ -1,6 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-
-export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '510b790e08fde7fb159a8b0a63c7acd7';
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
